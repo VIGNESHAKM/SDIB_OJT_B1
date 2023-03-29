6d1201a210205a4d006dd0f37c0f0b226e4a397a
VIGNESHAKM <114151574+VIGNESHAKM@users.noreply.github.com>
2023-03-29 17:05:44 +0530

    Add files via upload

---
+++ C_CODE/ARRAYS/pb3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+   float a[3]={18.5,20.3,14.5};
+   float i;
+    printf("enter the valu  ");
+    for(i=0;i<3;i++)
+    {
+        scanf("%d",&i, &a[3]);
+    }
+    printf("%d",a[3],a[3]);
+    }
